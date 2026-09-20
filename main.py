5f3c314d870bd5160f7ab8e6e07601df4a7e98ae
Michael St. Onge <31676847+mcsj120@users.noreply.github.com>
2026-09-20 13:38:27 -0700

    Main only runs if actually main

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import json
 
 from workflowy import workflowy_client as wf
 
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
-    username = config.get['username']
-    password = config.get['password']
+if __name__ == '__main__':
+    with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
+        username = config.get['username']
+        password = config.get['password']
 
-session_id = wf.WorkFlowyClient.login(username, password)
-client = wf.WorkFlowyClient(session_id)
+    session_id = wf.WorkFlowyClient.login(username, password)
+    client = wf.WorkFlowyClient(session_id)
 
-main = client.get_main_list()
-node = main.get_list_by_name_nested(['Mikey\'s Work', 'Mikey\'s Problems', 'Env', 'QA'])
-node.create_sublist(str(date.today()))
+    main = client.get_main_list()
+    node = main.get_list_by_name_nested(['Mikey\'s Work', 'Mikey\'s Problems', 'Env', 'QA'])
+    node.create_sublist(str(date.today()))
 
-problems = node.get_list_by_name(str(date.today()))
+    problems = node.get_list_by_name(str(date.today()))
